3121384ad791d664198386bf1877e9fb898ec8a1
Jonathan Germann <jonathan.germann@outlook.com>
2016-02-26 08:25:05 +0100

    feat(typeahead): new build

--- dist/ui-bootstrap-1.1.2-SNAPSHOT.js
+++ dist/ui-bootstrap-1.1.2-SNAPSHOT.js
@@ -2,7 +2,7 @@
  * angular-ui-bootstrap
  * http://angular-ui.github.io/bootstrap/
 
- * Version: 1.1.2-SNAPSHOT - 2016-02-03
+ * Version: 1.1.2-SNAPSHOT - 2016-02-26
  * License: MIT
  */angular.module("ui.bootstrap", ["ui.bootstrap.collapse","ui.bootstrap.accordion","ui.bootstrap.alert","ui.bootstrap.buttons","ui.bootstrap.carousel","ui.bootstrap.dateparser","ui.bootstrap.isClass","ui.bootstrap.position","ui.bootstrap.datepicker","ui.bootstrap.debounce","ui.bootstrap.dropdown","ui.bootstrap.stackedMap","ui.bootstrap.modal","ui.bootstrap.paging","ui.bootstrap.pager","ui.bootstrap.pagination","ui.bootstrap.tooltip","ui.bootstrap.popover","ui.bootstrap.progressbar","ui.bootstrap.rating","ui.bootstrap.tabs","ui.bootstrap.timepicker","ui.bootstrap.typeahead"]);
 angular.module('ui.bootstrap.collapse', [])
@@ -6344,6 +6344,9 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.debounce', 'ui.bootstrap
       if (!isEditable && modelCtrl.$error.editable) {
         modelCtrl.$viewValue = '';
         element.val('');
+      }
+      if (scope.activeIdx === -1 && !selected) {
+          modelCtrl.$setViewValue(null);
       }
       hasFocus = false;
       selected = false;
